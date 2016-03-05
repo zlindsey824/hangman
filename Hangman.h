75beca80e2a6e5b93b0e7f8f3fd8d5b2695f9404
matt ferg <mattmattferg@gmail.com>
2016-03-05 13:54:20 -0500

    add basic functions to hangman class

---
+++ Hangman.h
@@ -0,0 +1,19 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
+
+class HangMan
+{
+	public:
+		void displayHangman();
+		void displayGuesses();
+		void displayWord();	
+	private:
+		int maxWrong;
+		char *inputWord;
+};
+
+		
+		
+		
+		
+				
